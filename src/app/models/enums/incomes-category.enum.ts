43b13c886214e9933c642295c7caa94d3a7d12ac
Titowisk <rabelo51@gmail.com>
2025-12-06 08:39:32 -0300

    Refactor expenses and incomes categories to use string values for better readability

--- src/app/models/enums/incomes-category.enum.ts
+++ src/app/models/enums/incomes-category.enum.ts
@@ -1,8 +1,8 @@
 export enum IncomesCategory {
-    Salary = 1,
-    Benefits,
-    TaxReturns,
-    Bonus,
-    PaidVacation,
-    Others
+    Salary = 'Salary',
+    Benefits = 'Benefits',
+    TaxReturns = 'TaxReturns',
+    Bonus = 'Bonus',
+    PaidVacation = 'PaidVacation',
+    Others = 'Others'
 }
